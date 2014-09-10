7b5f04b40255ba2b3b125cc550669b5ce68e3b0a
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-09-10 16:21:15 -0400

    updated calculations for similarities and predicted ratings

--- movie_recommendations.rb
+++ movie_recommendations.rb
@@ -19,23 +19,8 @@ critics = {'Lisa Rose'=> {'Lady in the Water'=> 2.5, 'Snakes on a Plane'=> 3.5,
 
 you = {'Toby'=> {'Snakes on a Plane'=>4.5,'You, Me and Dupree'=>1.0,'Superman Returns'=>4.0}}
 
-
-def make_plots
-  graph_area =
-  [
-    [' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ']
-  ]
-end
-
-
 def sim_pearson(critics_hash, toby_hash)
   mutually_related_array = []
-
-  toby_hash = {'Snakes on a Plane'=>4.5,'You, Me and Dupree'=>1.0,'Superman Returns'=>4.0}
 
   critics_hash.each do |critic_movie, critic_rating|
     toby_hash.each do |toby_movie, toby_rating|
@@ -69,12 +54,128 @@ def sim_pearson(critics_hash, toby_hash)
   return (numerator / denominator)
 end
 
-critics.each do |each_critic, value|
-  p each_critic.to_s + ": " + sim_pearson(value, you['Toby']).to_s
+def find_my_unrated_items(critic, toby_hash)
+  unique_array = []
+  critic.each do |each_movie, rating|
+    unique_array << each_movie
+  end
+  unique_array.uniq!
+  toby_array = []
+  toby_hash["Toby"].each do |each_movie, each_rating|
+    toby_array << each_movie
+  end
+
+  return unique_array - toby_array
 end
 
-
-def hash_of_weighted_scores(critic, sim_pearson_score)
-
-
+def expected_values_for_unseen_movies_by_critic(critic, you)
+  expected_values_for_unseen_movies = Hash.new
+  # output should equal {"Lady in the Water" => 2.5*0.99, "Just My Luck" => 3*0.99, "The Night Listener" => 3*0.99}
+  # }
+  sim_pearson_score = sim_pearson(critic, you['Toby'])
+  find_my_unrated_items(critic, you).each do |each_movie|
+    expected_values_for_unseen_movies[each_movie] = critic[each_movie] * sim_pearson_score
+  end
+  return expected_values_for_unseen_movies
 end
+
+def expected_values_for_unseen_movies_for_all_critics(critics, you)
+
+  # output should equal {'Lisa Rose' => {"Lady in the Water" => 2.5*0.99, "Just My Luck" => 3*0.99, "The Night Listener" => 3*0.99},
+  #   {'Claudia Puig' => {"Just My Luck" => 0.89*3.0, "The Night Listener" => 0.89*4.5}
+  # }
+
+  output = Hash.new
+  output_array = []
+  critics.each do |each_critic, value|
+    output_array << expected_values_for_unseen_movies_by_critic(critics[each_critic], you)
+  end
+
+return output_array
+end
+
+def create_hash_of_expected_ratings(critics, you)
+  b = Hash.new([])
+
+  expected_values_for_unseen_movies_for_all_critics(critics, you).each do |hash|
+    hash.each do |each_movie, each_rating|
+      b[each_movie] += [each_rating] if each_rating > 0
+    end
+  end
+
+  return b
+end
+
+def hash_of_sims_of_all_critics(critics, you)
+  output = Hash.new(0)
+  critics.each do |each_critic, value|
+    output[each_critic] = sim_pearson(value, you['Toby'])
+  end
+  output
+end
+
+def has_critic_reviewed_a_movie?(critic, critics, movie_title)
+  critics[critic].has_key?(movie_title)
+end
+
+def sim_sum_per_movie(hash_of_sims_of_all_critics, create_hash_of_expected_ratings, critics)
+  #output should be {"Lady in the Water" => [0.99, 0.38, 0.92, 0.66], "Just My Luck" => [0.99, 0.38, 0.89, 0.92], "The Night Listener" => [0.99, 0.38, 0.89, 0.92, 0.66]}
+
+  output = Hash.new([])
+  critics.each do |each_critic, value|
+    value.each do |each_movie, each_rating|
+      output[each_movie] += [hash_of_sims_of_all_critics[each_critic]] if hash_of_sims_of_all_critics[each_critic] > 0
+    end
+  end
+
+  return output
+end
+
+def movie_rating_predictions_hash(create_hash_of_expected_ratings, sim_sum_per_movie)
+  output = Hash.new(0)
+  create_hash_of_expected_ratings.each do |each_movie, array_of_expected_ratings|
+    output[each_movie] = array_of_expected_ratings.inject(:+)
+  end
+
+  output_sims = Hash.new(0)
+  sim_sum_per_movie.each do |each_movie, sims|
+    output_sims[each_movie] = sims.inject(:+)
+  end
+
+  final_output = Hash.new(0)
+  output.each do |each_movie, sum|
+    final_output[each_movie] = output[each_movie] / output_sims[each_movie].to_f
+  end
+
+  final_output
+end
+
+puts
+puts "===== pearson sim scores for each critic: hash_of_sims_of_all_critics ===="
+puts
+
+p hash_of_sims_of_all_critics(critics, you)
+
+puts
+puts "===== array of expectations: expected_values_for_unseen_movies_for_all_critics====="
+puts
+
+p expected_values_for_unseen_movies_for_all_critics(critics, you)
+
+puts
+puts "===== hash of expected ratings: create_hash_of_expected_ratings ===="
+puts
+
+p create_hash_of_expected_ratings(critics, you)
+
+puts
+puts "===== sim sum per movie ===="
+puts
+
+p sim_sum_per_movie(hash_of_sims_of_all_critics(critics, you), create_hash_of_expected_ratings(critics,you), critics)
+
+puts
+puts "===== movie_ratings_predictions_hash ===="
+puts
+
+p movie_rating_predictions_hash(create_hash_of_expected_ratings(critics, you), sim_sum_per_movie(hash_of_sims_of_all_critics(critics, you), create_hash_of_expected_ratings(critics,you), critics))
