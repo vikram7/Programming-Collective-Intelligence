64a41d4a2f4325fafd507095f6766291a68d05e6
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-09-10 19:36:01 -0400

    added user inputs, but need to sanitize them

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Command-line based movie recommendation app based on Toby Segaran's Programming Collective Intelligence. Movie recommendations are based on a collaborative filtering algorithm (Pearson similarity). Written in Ruby.
 
 To-do:
-- update user/recommendation source file with MovieLens data
+- sanitize user input
 - Refactor algorithm code
--- movie_recommendations.rb
+++ movie_recommendations.rb
@@ -1,55 +1,6 @@
 require 'pry'
 require 'csv'
 
-# critics = {'Lisa Rose'=> {'Lady in the Water'=> 2.5, 'Snakes on a Plane'=> 3.5,
-#       'Just My Luck'=> 3.0, 'Superman Returns'=> 3.5, 'You, Me and Dupree'=> 2.5,
-#       'The Night Listener'=> 3.0},
-#      'Gene Seymour'=> {'Lady in the Water'=> 3.0, 'Snakes on a Plane'=> 3.5,
-#       'Just My Luck'=> 1.5, 'Superman Returns'=> 5.0, 'The Night Listener'=> 3.0,
-#       'You, Me and Dupree'=> 3.5},
-#      'Michael Phillips'=> {'Lady in the Water'=> 2.5, 'Snakes on a Plane'=> 3.0,
-#       'Superman Returns'=> 3.5, 'The Night Listener'=> 4.0},
-#      'Claudia Puig'=> {'Snakes on a Plane'=> 3.5, 'Just My Luck'=> 3.0,
-#       'The Night Listener'=> 4.5, 'Superman Returns'=> 4.0,
-#       'You, Me and Dupree'=> 2.5},
-#      'Mick LaSalle'=> {'Lady in the Water'=> 3.0, 'Snakes on a Plane'=> 4.0,
-#       'Just My Luck'=> 2.0, 'Superman Returns'=> 3.0, 'The Night Listener'=> 3.0,
-#       'You, Me and Dupree'=> 2.0},
-#      'Jack Matthews'=> {'Lady in the Water'=> 3.0, 'Snakes on a Plane'=> 4.0,
-#       'The Night Listener'=> 3.0, 'Superman Returns'=> 5.0, 'You, Me and Dupree'=> 3.5}}
-
-you = {'Toby'=> {242=>3,
-   393=>4,
-   381=>4,
-   251=>3,
-   655=>5,
-   67=>5,
-   306=>4,
-   238=>4,
-   663=>5,
-   111=>4,
-   580=>2,
-   25=>4,
-   286=>5,
-   94=>3,
-   692=>5,
-   8=>5,
-   428=>4,
-   1118=>4,
-   70=>3,
-   66=>3,
-   257=>2,
-   108=>4,
-   202=>3,
-   340=>3,
-   287=>3,
-   116=>3,
-   382=>4,
-   285=>5,
-   1241=>3,
-   1007=>4,
-   411=>4}
- }
 
 parsed_file = CSV.read('./100_000_ratings/u.data', { :col_sep => "\t"})
 parsed_items = CSV.read('./100_000_ratings/u.item', { :col_sep => "|"})
@@ -67,6 +18,12 @@ end
 
 parsed_items.each do |each_row|
   id_to_movie_hash[each_row[0].to_i] = each_row[1]
+end
+
+movie_to_id_hash = Hash.new
+
+id_to_movie_hash.each do |key, value|
+  movie_to_id_hash[value] = key
 end
 
 def sim_pearson(critics_hash, toby_hash)
@@ -200,45 +157,73 @@ def movie_rating_predictions_hash(create_hash_of_expected_ratings, sim_sum_per_m
   final_output
 end
 
-puts
-puts "===== pearson sim scores for each critic: hash_of_sims_of_all_critics ===="
-puts
+# puts
+# puts "===== pearson sim scores for each critic: hash_of_sims_of_all_critics ===="
+# puts
 
-p hash_of_sims_of_all_critics(critics, you)
+# p hash_of_sims_of_all_critics(critics, you)
 
-puts
-puts "===== array of expectations: expected_values_for_unseen_movies_for_all_critics====="
-puts
+# puts
+# puts "===== array of expectations: expected_values_for_unseen_movies_for_all_critics====="
+# puts
 
-p expected_values_for_unseen_movies_for_all_critics(critics, you)
+# p expected_values_for_unseen_movies_for_all_critics(critics, you)
 
-puts
-puts "===== hash of expected ratings: create_hash_of_expected_ratings ===="
-puts
+# puts
+# puts "===== hash of expected ratings: create_hash_of_expected_ratings ===="
+# puts
 
-p create_hash_of_expected_ratings(critics, you)
+# p create_hash_of_expected_ratings(critics, you)
 
-puts
-puts "===== sim sum per movie ===="
-puts
+# puts
+# puts "===== sim sum per movie ===="
+# puts
 
-p sim_sum_per_movie(hash_of_sims_of_all_critics(critics, you), create_hash_of_expected_ratings(critics,you), critics)
+# p sim_sum_per_movie(hash_of_sims_of_all_critics(critics, you), create_hash_of_expected_ratings(critics,you), critics)
 
-puts
-puts "===== movie_ratings_predictions_hash ===="
-puts
+# puts
+# puts "===== movie_ratings_predictions_hash ===="
+# puts
 
-p movie_rating_predictions_hash(create_hash_of_expected_ratings(critics, you), sim_sum_per_movie(hash_of_sims_of_all_critics(critics, you), create_hash_of_expected_ratings(critics,you), critics))
+# p movie_rating_predictions_hash(create_hash_of_expected_ratings(critics, you), sim_sum_per_movie(hash_of_sims_of_all_critics(critics, you), create_hash_of_expected_ratings(critics,you), critics))
 
-puts
-puts "===== sorted results ===="
-puts
+# puts
+# puts "===== sorted results ===="
+# puts
 
-p movie_rating_predictions_hash(create_hash_of_expected_ratings(critics, you), sim_sum_per_movie(hash_of_sims_of_all_critics(critics, you), create_hash_of_expected_ratings(critics,you), critics)).sort_by {|movie, predicted_rating| predicted_rating}
+# p movie_rating_predictions_hash(create_hash_of_expected_ratings(critics, you), sim_sum_per_movie(hash_of_sims_of_all_critics(critics, you), create_hash_of_expected_ratings(critics,you), critics)).sort_by {|movie, predicted_rating| predicted_rating}
 
-puts
-puts "==== We recommend you see the following 20 movies ===="
-puts
+# puts
+# puts "==== We recommend you see the following 20 movies ===="
+# puts
+
+puts "Ok, we need you to rate 20 movies before we can give you a good recommendation, so bear with us!"
+input_hash = Hash.new
+
+for i in 0..19
+  puts "#{20-i} left to rate!!!"
+
+  out_hash = Hash.new
+  puts "Enter a movie you want to rate"
+  movie = gets.chomp.to_s
+  movie_to_id_hash.keys.each do |x|
+    if x.downcase.include? movie.downcase
+      puts movie_to_id_hash[x].to_s + ": " + x
+    end
+  end
+
+  puts "Enter the number of the movie you want to rate"
+  movie_num = gets.chomp.to_i
+  puts
+
+  puts "What do you want to rate it (1-5)"
+  rating = gets.chomp.to_f
+  puts
+
+  input_hash[movie_num] = rating
+end
+
+you = {'Toby'=> input_hash}
 
 a = movie_rating_predictions_hash(create_hash_of_expected_ratings(critics, you), sim_sum_per_movie(hash_of_sims_of_all_critics(critics, you), create_hash_of_expected_ratings(critics,you), critics)).sort_by {|movie, predicted_rating| -predicted_rating}
 
